d3aedd4bac647a1906cf7a741eb7bea8949db184
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-08 18:14:59 +0200

    add function getComputerChoice to script.js file

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+console.log(getComputerChoice())
+
+function getComputerChoice() {
+    
+    let randomValue = (Math.random() * 10);
+
+    return computerChoice = (randomValue >= 6.66 ? "Scissors" : 
+                             randomValue >= 3.33 ? "Paper"    :
+                             randomValue >  0    ? "Rock"     : "e");
+}
