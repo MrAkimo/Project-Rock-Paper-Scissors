6a1b3f960510232f39782b80daf2eb2e19015e93
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-08 18:55:30 +0200

    add function getHumanChoice to file script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-console.log(getComputerChoice())
+console.log(getComputerChoice());
+console.log(getHumanChoice());
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose: Rock, Paper, Scissors")
+    return humanChoice.toLowerCase();
+}
 
 function getComputerChoice() {
-    
+
     let randomValue = (Math.random() * 10);
 
     return computerChoice = (randomValue >= 6.66 ? "Scissors" : 
