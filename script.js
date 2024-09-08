463ab67c804ffdaef6b90909d8d394ae457ec1fa
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-08 19:36:51 +0200

    add function playRound to script.js file

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+let humanScore = 0
+let computerScore = 0
+
+//console.log(getComputerChoice());
+//console.log(getHumanChoice());
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(`You Score: ${humanScore}`);
+console.log(`Computer Score: ${computerScore}`);
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" || 
+        humanChoice == "paper" || 
+        humanChoice == "scissors") {
+
+            if (humanChoice == computerChoice) {
+                console.log("Draw")
+                return
+            }
+        
+            if (humanChoice == "rock" && computerChoice == "scissors" || 
+                humanChoice == "scissors" && computerChoice == "paper" || 
+                humanChoice == "paper" && computerChoice == "rock") {
+        
+                console.log("You Win");
+                humanScore += 1;
+            } else {
+                console.log("You Lose");
+                computerScore += 1;
+            }
+    } else {
+        console.log("Invalid Choice");
+    }
+
+}
 
 function getHumanChoice() {
     let humanChoice = prompt("Choose: Rock, Paper, Scissors")
@@ -10,7 +46,7 @@ function getComputerChoice() {
 
     let randomValue = (Math.random() * 10);
 
-    return computerChoice = (randomValue >= 6.66 ? "Scissors" : 
-                             randomValue >= 3.33 ? "Paper"    :
-                             randomValue >  0    ? "Rock"     : "e");
+    return computerChoice = (randomValue >= 6.66 ? "scissors" : 
+                             randomValue >= 3.33 ? "paper"    :
+                             randomValue >  0    ? "rock"     : "e");
 }
