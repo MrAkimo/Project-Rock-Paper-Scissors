6a5b61d08fbcde1819bf135612b92981d10f267b
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-10-20 12:53:30 +0200

    removed old code add who wins

--- script.js
+++ script.js
@@ -2,76 +2,43 @@ let playerScore = 0;
 let botScore = 0;
 let drawsScore = 0;
 
-// Events
-const btn_rock = document.querySelector("#btn-rock");
-const btn_paper = document.querySelector("#btn-paper");
-const btn_scissors = document.querySelector("#btn-scissors");
-const p_display = document.querySelector('#p-display');
-const buttonsNodeList = document.querySelectorAll('button');
-const buttons = [...buttonsNodeList];
-
-// btn_rock.addEventListener("click", () => {
-//     playRound('rock', getComputerChoice());
-// })
-
-// btn_paper.addEventListener("click", () => {
-//     playRound('paper', getComputerChoice());
-// })
-
-// btn_scissors.addEventListener("click", () => {
-//     playRound('scissors', getComputerChoice());
-// })
-
-
-
-//console.log(getComputerChoice());
-//console.log(getHumanChoice());
-
-
-//playRound(humanSelection, computerSelection);
 playGame();
 
 function displayChange(textValue) {
+
+    let p_display = document.querySelector('#p-display');
     p_display.textContent = textValue;
 }
 function updateScore(whoWon){
-    const youScoreText = document.querySelector('#you-score');
-    const botScoreText = document.querySelector('#bot-score');
-    const drawsScoreText = document.querySelector('#draws-score');
 
-    whoWon == 'player' ? youScoreText.textContent = `You: ${playerScore + 1}`:
-    whoWon == 'bot' ? botScoreText.textContent = `Bot: ${botScore + 1}` :
-    whoWon == 'draw' ? drawsScoreText.textContent = `Draws: ${drawsScore + 1}` : ''
+    let youScoreText = document.querySelector('#you-score');
+    let botScoreText = document.querySelector('#bot-score');
+    let drawsScoreText = document.querySelector('#draws-score');
+
+    whoWon == 'player' ? youScoreText.textContent = `You: ${playerScore += 1}`:
+    whoWon == 'bot' ? botScoreText.textContent = `Bot: ${botScore += 1}` :
+    whoWon == 'draw' ? drawsScoreText.textContent = `Draws: ${drawsScore += 1}` : ''
 
 
 }
 
 function playGame() {
-    let round = 1
+    
+    const buttonsNodeList = document.querySelectorAll('button');
+    const buttons = [...buttonsNodeList];
     
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            if(round < 5) {
+            if(playerScore < 5  && botScore < 5) {
                 playRound(button.textContent.toLowerCase(), getComputerChoice())
-                round += 1;
             }
-            console.log(round)
+            if(playerScore == 5  || botScore == 5){
+                playerScore > botScore ? displayChange('You Win the Game') : 
+                playerScore == botScore ? displayChange('You Draw the Game') : 
+                                               displayChange('You Lost the Game')
+            }
         })
     })
-    // for( let i = 0; i < 5; i++) {
-    //     let humanSelection = getHumanChoice();
-    //     let computerSelection = getComputerChoice();
-
-    //     playRound(humanSelection, computerSelection);
-
-    //     console.log(`You Score: ${humanScore}`);
-    //     console.log(`Computer Score: ${computerScore}`);
-
-    // }
-
-    // humanScore > computerScore ? console.log("You Win the Game") : 
-    // humanScore == computerScore ? console.log("You Draw the Game") : 
-    //                                 console.log("You Lost the Game")
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -92,14 +59,11 @@ function playRound(humanChoice, computerChoice) {
                 displayChange("You Won the Round");
                 updateScore('player');
             } else {
-                displayChange("You Lose");
+                displayChange("You Lose the Round");
                 updateScore('bot');
             }
             
-    } else {
-        displayChange("You Lost the Round");
     }
-
 }
 
 function getHumanChoice() {
